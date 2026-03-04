ed4b9a6c750845ae0f00773709f2ca23c6b014d3
ShaneHmn <shanehumann@gmail.com>
2026-03-04 09:38:10 -0800

    loading

---
+++ js/loading.js
@@ -0,0 +1,18 @@
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 25)
+
+function blurring() {
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadtext.innerText = `${load}%`
+    loadText.style.opacity = 1 - load/100
+
+    bg.style.filter = `blur(${30-(load/100)*30}px)`
+}

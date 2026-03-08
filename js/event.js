f995a21845bbe8d5491a0c79298e6118e00fcbb4
ShaneHmn <shanehumann@gmail.com>
2026-03-08 06:10:14 +0000

    Sat, Mar 7, 2026, 10:10 PM -08:00

--- js/event.js
+++ js/event.js
@@ -1,4 +1,5 @@
 const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
 
 element1.addEventListener('click', ()=>{
     element1.innerHTML = "I've been clicked! <br> Try double clicking me."

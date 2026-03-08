63a3aca2104f710cf61b2d24b5ac5006aa37b34b
ShaneHmn <shanehumann@gmail.com>
2026-03-08 06:23:57 +0000

    Sat, Mar 7, 2026, 10:23 PM -08:00

--- js/event.js
+++ js/event.js
@@ -21,8 +21,13 @@ element2.addEventListener('mouseout', ()=>{
     element2.style.backgroundcolor = "#BB33BB"
 })
 
-
+let positionY = 0;
+let positionX = 0;
 
 document.addEventListener('keyup', (event)=>{
     console.log(event.key)
+
+    if(event.key == 'ArrowDown') {
+
+    }
 })

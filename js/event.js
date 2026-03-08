315367f4e3b7ed1e81f3fa3fe2a9c1d95ac37a3c
ShaneHmn <shanehumann@gmail.com>
2026-03-08 06:43:08 +0000

    Sat, Mar 7, 2026, 10:43 PM -08:00

--- js/event.js
+++ js/event.js
@@ -52,3 +52,16 @@ element4.addEventListener('click', ()=>{
     element4.innerHTML = ""
     element4.style.opacity = .3
 })
+
+
+document.addEventListener('keyup', (event)=>{
+    if (event.key == 'j'){
+        element4.style.width = "300px"
+        element4.style.height = "50px"
+        element4.style.opacity = 1
+        element4.innerHTML = "<h4>Click me to make me small. <br> Press 'j' to bring me back.</h4>"
+    }
+})
+
+
+const list = ['running', 'paused']

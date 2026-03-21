b51040e2ccb8f5899e655d28d86c21b2dbab7bd1
ShaneHmn <shanehumann@gmail.com>
2026-03-21 21:28:39 +0000

    Sat, Mar 21, 2026, 2:28 PM -07:00

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,9 @@
 nav = document.querySelector('.nav')
 
-window.addEventListener('scroll', fixNav())
+window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log('hello')
+    console.log(window.scrollY)
     if (window.scrollY > 223) {
         nav.classList.add('active')
     } else {

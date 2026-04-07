917ee49d409519e7f1fac7f821d3f03d5bbbe274
ShaneHmn <shanehumann@gmail.com>
2026-04-07 15:21:09 +0000

    Tue, Apr 7, 2026, 8:21 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 secret = document.querySelector('.secret')
 
-secret.addEventListener('mouseover', ()=>{
+secret.addEventListener('mouseover', ()=> {
     secret.style.color = "yellow"
 })
 
-secret.addEventListener('mouseout', ()=>{
+secret.addEventListener('mouseout', ()=> {
     secret.style.color = "rgb(121,28,158)"
 })
